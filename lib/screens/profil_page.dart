e45f3a5815256a3296eafc83e80f0a02d101ad19
rsamirdevone <fokinartemorgin@gmail.com>
2023-02-19 17:57:26 +0500

    New Version

--- lib/screens/profil_page.dart
+++ lib/screens/profil_page.dart
@@ -7,10 +7,12 @@ class ProfilPages extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final BlackColor = Colors.black54;
     return Scaffold(
       backgroundColor: Colors.grey.shade300,
       bottomNavigationBar: CurvedNavigationBar(
         backgroundColor: Colors.grey.shade400,
+        // ignore: prefer_const_literals_to_create_immutables
         items: [
           CurvedNavigationBarItem(
             child: Icon(Icons.home_outlined),
@@ -38,54 +40,125 @@ class ProfilPages extends StatelessWidget {
         title: Text('Profile'),
         backgroundColor: Colors.deepOrange,
       ),
-      body: Container(
-        child: Column(
-          children: [
-            SizedBox(
-              height: 20,
+      body: Column(
+        children: [
+          SizedBox(
+            height: 35,
+          ),
+          Center(
+            child: CircleAvatar(
+              radius: 60,
+              backgroundImage: NetworkImage(
+                  'https://sun1.userapi.com/sun1-28/s/v1/ig2/mJ7qcq-WSJweLXkEEog_vlRHIa8nbzTXGXgvXna45Mt1RcOhjrLOHpq_Lcx_I1kXzR55sawvLW6L5QpHM_z8vpZJ.jpg?size=510x510&quality=95&type=album'),
             ),
-            SizedBox(
-              width: 300,
+          ),
+          const SizedBox(
+            height: 13.0,
+          ),
+          Row(
+            children: [
+              Padding(
+                padding: EdgeInsets.only(left: 122),
+              ),
+              Text(
+                'Raupov Samir',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                width: 5,
+              ),
+              Icon(
+                Icons.verified_rounded,
+                color: Colors.blue[600],
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 5.0,
+          ),
+          Text(
+            'raupovsamir@gmail.com',
+            style: TextStyle(
+              color: Colors.black38,
             ),
-            Center(
-              child: CircleAvatar(
-                backgroundImage: NetworkImage(
-                    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSzHQv_th9wq3ivQ1CVk7UZRxhbPq64oQrg5Q&usqp=CAU'),
-                radius: 70,
+          ),
+          SizedBox(
+            height: 30.0,
+          ),
+          Container(
+            child: Padding(
+              padding: const EdgeInsets.only(
+                left: 20,
+                right: 20,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Icon(Icons.settings),
+                  Icon(Icons.arrow_forward),
+                ],
               ),
             ),
-            SizedBox(
-              height: 20,
-            ),
-            Text(
-              'Raupov Samir',
-              style: TextStyle(
-                fontSize: 25,
-                fontWeight: FontWeight.bold,
+          ),
+          SizedBox(
+            height: 30.0,
+          ),
+          Container(
+            child: Padding(
+              padding: const EdgeInsets.only(
+                left: 20,
+                right: 20,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Icon(Icons.settings),
+                  Icon(Icons.arrow_forward),
+                ],
               ),
             ),
-            SizedBox(
-              height: 10,
-            ),
-            Text(
-              '+998 90 777 11 11',
-              style: TextStyle(
-                fontSize: 25,
-                fontWeight: FontWeight.bold,
+          ),
+          SizedBox(
+            height: 30.0,
+          ),
+          Container(
+            child: Padding(
+              padding: const EdgeInsets.only(
+                left: 20,
+                right: 20,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Icon(Icons.settings),
+                  Icon(Icons.arrow_forward),
+                ],
               ),
             ),
-            SizedBox(
-              height: 10,
-            ),
-            Text(
-              'raupovsamir@gmail.com',
-              style: TextStyle(
-                fontSize: 25,
-                fontWeight: FontWeight.bold,
+          ),
+          SizedBox(
+            height: 30.0,
+          ),
+          Container(
+            child: Padding(
+              padding: const EdgeInsets.only(
+                left: 20,
+                right: 20,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Icon(Icons.settings),
+                  Icon(Icons.arrow_forward),
+                ],
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
